5a5d5c5abb0d04e321127222665a913354d36e84
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2023-02-14 11:23:23 +0400

    Changed Done

--- src/main/java/com/mySystem/Main.java
+++ src/main/java/com/mySystem/Main.java
@@ -1,16 +1,18 @@
 package com.mySystem;
+
 /**
- * Main class is responsible for sending a request to Google Distance Matrix API and handling the response.
+ * The main class of the program.
  */
 public class Main {
     /**
-     * The main method sends the request to the API and handles the response.
-     * @param args command line arguments
+     * The main method of the program. Creates a {@link RequestMaker} object and calls its
+     * {@link RequestMaker#makeRequest(String[], String[])} method with the command line arguments.
+     * @param args the command line arguments. The first argument should be of the form "parameter:value"
+     *             and specify the origin location. The second argument should be of the same form and
+     *             specify the destination location.
      */
     public static void main(String[] args) {
-
         RequestMaker requestMaker = new RequestMaker();
-
-        requestMaker.makeRequest(args[0].split(":") , args[1].split(":"));
+        requestMaker.makeRequest(args[0].split(":"), args[1].split(":"));
     }
 }
